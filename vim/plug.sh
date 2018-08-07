dd4b8e4e9cb995e0dabe3790cebc97dfc1989283
Steffen Haug <steffenandr@gmail.com>
2018-08-07 12:28:47 +0200

    reduced usage of awk, should be both faster and cleaner

--- vim/plug.sh
+++ vim/plug.sh
@@ -1,30 +1,25 @@
 #! /bin/bash
 configfile=${1:-$HOME/dotfiles/vim/plugs.txt}
 
-# mini "parser" using awk:
-#  - skips empty lines (^$)
-#  - skips lines starting with "#" (#.*$)
-#  - get the folder name from the url to check if it already exists
-awk '
-/#.*$/ {next}
-/^$/   {next}
-       {x=$1; n=split(x, xs, "/"); print xs[n] " " x}' $configfile |
-           while read line; do
-               dir=$(echo $line | awk '{print $1}')
-               absolute_dir=$HOME/.vim/bundle/$dir
+# remove comments and empty lines
+clean_cfg=$(awk '/#.*$/{next} /^$/{next} {print $1}' $configfile)
 
-               repository=$(echo $line | awk '{print $2}')
-               url=https://github.com/$repository
+for line in $clean_cfg; do
+    # get the folder name by splitting on /, and selecting the last word
+    dir=$(echo $line | awk -F/ '{print $NF}')
+    absolute_dir=$HOME/.vim/bundle/$dir
 
-               if [ -d $absolute_dir ]; then
-                   echo "$dir already exists. Updating:"
-                   cd $absolute_dir
-                   git pull
-               else
-                   echo "$dir not found. Installing:"
-                   git clone $url $absolute_dir
-               fi
-           done
+    url=https://github.com/$line
+
+    if [ -d $absolute_dir ]; then
+        echo "$dir already exists. Updating:"
+        cd $absolute_dir
+        git pull
+    else
+        echo "$dir not found. Installing:"
+        git clone $url $absolute_dir
+    fi
+done
 
 echo "Done updating! Cleaning up if necessary."
 
@@ -33,11 +28,7 @@ cd $HOME/.vim/bundle
 for line in $(ls); do
     # use a simmilar awk program to strip the comments,
     # and search for the folder in it.
-    if ! awk '
-        /#.*$/ {next}
-        /^$/   {next}
-               {print $1}' $configfile |
-                   grep -q "/$line";
+    if ! grep -q "/$line" <<< $clean_cfg;
     then
         # if the folder is *not* in the config, we remove it.
         echo "$line is in bundle, but not in config. Deleting."
